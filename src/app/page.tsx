ba7f8c3b241b0f8a8c21cb30148ed550253b00d8
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-21 00:01:09 -0300

    implementação calendario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
+"use client"
+import React, { useState } from "react";
 import Link from "next/link";
 import HeaderPrincipal from "@/components/header";
 import BoxUserPrincipal from "@/components/box-user-principal";
 import NovoAgendamentoPrincipal from "@/components/box-novo-agendamento";
+import { Calendar } from "@/components/ui/calendar"
+
 export default function Home() {
+
+  const [date, setDate] = useState<Date>(new Date())
+
   return (
     <main>
-      <HeaderPrincipal/>
+      <HeaderPrincipal />
       <Link href={'/login'}>ir para login</Link>
-      <BoxUserPrincipal/>
-      <NovoAgendamentoPrincipal/>
+      <BoxUserPrincipal />
+      <NovoAgendamentoPrincipal />
+      <Calendar
+        mode="single"
+        selected={date}
+        className="rounded-md border w-[90vw] mx-auto" 
+        style={{backgroundColor: "white"}}/>
     </main>
   );
 }
