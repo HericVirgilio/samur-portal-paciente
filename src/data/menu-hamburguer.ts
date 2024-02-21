e74145e9affbd500cf4677d8a989673b81f60714
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-21 13:03:18 -0300

    importação dos icones menuHambuerguer criação do dto e applicação do onClick

---
+++ src/data/menu-hamburguer.ts
@@ -0,0 +1,44 @@
+export const menuHamburguer = [
+    {
+        key: 1,
+        nome: "Home",
+        nomeUrl: "",
+        url: "http://localhost:3000/icons/home.svg"
+    },
+    {
+        key: 2,
+        nome: "Agendamento",
+        nomeUrl: "/agendamento",
+        url: "http://localhost:3000/icons/calendarioMenu.svg"
+    },
+    {
+        key: 3,
+        nome: "Documentos",
+        nomeUrl: "/documentos",
+        url: "http://localhost:3000/icons/documentosHamburguer.svg"
+    },
+    {
+        key: 4,
+        nome: "Financeiro",
+        nomeUrl: "/financeiro",
+        url:"http://localhost:3000/icons/financeiroMenu.svg"
+    },
+    {
+        key: 5,
+        nome: "Orçamento",
+        nomeUrl: "orçamento",
+        url: "http://localhost:3000/icons/oraçamentoMenu.svg"
+    },
+    {
+        key: 6,
+        nome: "Prontuário",
+        nomeUrl: "prontuario",
+        url: "http://localhost:3000/icons/prontuario.svg"
+    },
+    {
+        key: 7,
+        nome: "Resultados",
+        nomeUrl: "resultados",
+        url: "http://localhost:3000/icons/resultados"
+    }
+]
