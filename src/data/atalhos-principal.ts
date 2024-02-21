1a9df697d6f80593cc5290b7e4ea203dc38026be
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-21 11:07:35 -0300

    adicionando icones e criação do data para menu e proximo evento

---
+++ src/data/atalhos-principal.ts
@@ -0,0 +1,26 @@
+export const AtalhosPrincipal = [
+    {   
+        key: 1,
+        nome: "Atestado",
+        nomeUrl: "prontuario/atestado",
+        url: "http://localhost:3000/icons/atestado.svg"
+    },
+    {
+        key: 2,
+        nome: "TeleConsulta",
+        nomeUrl: "prontuario/teleconsulta",
+        url: "http://localhost:3000/icons/computer.svg"
+    },
+    {
+        key: 3,
+        nome: "Receitas",
+        nomeUrl: "prontuario/receitas",
+        url: "http://localhost:3000/icons/receita-simbolo.svg"
+    },
+    {
+        key: 4,
+        nome: "Financeiro",
+        nomeUrl: "financeiro",
+        url: "http://localhost:3000/icons/financeiro.svg"
+    }
+]
---
+++ src/data/proximo-evento.ts
@@ -0,0 +1,6 @@
+export const lista = 
+    {
+        exameNome: "Eletroencefalograma (EEG) - Um exame que registra a atividade elétrica do cérebro.",
+        data: "18/03/2023",
+        hora: "15:30"
+    }
