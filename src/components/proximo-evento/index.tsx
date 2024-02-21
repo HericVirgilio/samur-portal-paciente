926895fca5893012fa5e762fac1a05b2c144285e
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-21 11:08:32 -0300

    implementação proximo evento

---
+++ src/components/proximo-evento/index.tsx
@@ -0,0 +1,21 @@
+import "./style.css"
+import { lista } from "@/data/proximo-evento"
+import Image from "next/image"
+export default function ProximoEvento(){
+    return(
+        <div className="BoxProximoEvento">
+            <div className="BoxtituloProximoEvento">
+                <h3>Próximo evento</h3>
+            </div>
+            <div className="BoxProximoEventoDetalhes">
+                <p className="tituloEvento">{lista.exameNome}</p>
+                <div className="BoxDataHora">
+                    <Image src="/icons/calendario.svg" alt="" width={20} height={20}/>
+                    <p>{lista.data}</p>
+                    <Image src="/icons/relogio.svg" alt="" width={20} height={20}/>
+                    <p>{lista.hora}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
