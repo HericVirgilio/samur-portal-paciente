7c265430308a71373d7daed40b009773790c06e7
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-21 11:11:43 -0300

    alterações na pagina principal e ao cssglobal e criação de um style.css para o page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,30 @@
 "use client"
 import React, { useState } from "react";
-import Link from "next/link";
 import HeaderPrincipal from "@/components/header";
 import BoxUserPrincipal from "@/components/box-user-principal";
 import NovoAgendamentoPrincipal from "@/components/box-novo-agendamento";
 import { Calendar } from "@/components/ui/calendar"
-
+import ProximoEvento from "@/components/proximo-evento";
+import Atalhos from "@/components/Atalhos";
+import "./style.css";
+import BotaoGoogleAgenda from "@/components/botao-google-agenda";
 export default function Home() {
 
   const [date, setDate] = useState<Date>(new Date())
 
   return (
-    <main>
+    <main className="BoxPaginaPrincipal">
       <HeaderPrincipal />
       <BoxUserPrincipal />
       <NovoAgendamentoPrincipal />
+      <Atalhos/>
+      <ProximoEvento/>
       <Calendar
         mode="single"
         selected={date}
         className="rounded-md border w-[90vw] mx-auto"
         style={{ backgroundColor: "white" }} />
+        <BotaoGoogleAgenda/>
     </main>
   );
 }
