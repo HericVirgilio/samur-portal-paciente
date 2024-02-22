52bd7f317d1c29a7880297297e73d21eefa08646
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-22 08:59:24 -0300

    menu hamburguer implementado falta passar atalhos e componenets por proxy

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,8 @@
 import './style.css'
 import Image from 'next/image'
 import { useState } from 'react'
+import Link from 'next/link';
+import { menuHamburguerData } from '@/data/menu-hamburguer';
 export default function HeaderPrincipal() {
     const[menu, setMenu] = useState({});
     const menuHamburguer = () => {
@@ -28,15 +30,22 @@ export default function HeaderPrincipal() {
                     <Image src="/icons/close.svg" alt='close' height={25} width={25} onClick={fecharMenu}/>
                     <Image src="/images/samur-secundaria.png" alt='samur logo' width={100} height={75}/>
                 </div>
-                <ul>
-                    <li>Home</li>
-                    <li>Agendamento</li>
-                    <li>Documentos</li>
-                    <li>Financeiro</li>
-                    <li>Orçamento</li>
-                    <li>Prontuário</li>
-                    <li>Resultados</li>
-                </ul>
+
+                <div className='BoxMapMenuHamburguer' onClick={fecharMenu}>
+                        <Image src="/icons/home.svg" alt="home" height={40} width={40}/>
+                        <p className='TextoMapMenuHamburguer'>Inicio</p>
+                    </div>
+
+                {menuHamburguerData.map((objeto) => 
+                    <Link className='BoxMapMenuHamburguer' key={objeto.key} href={`${objeto.nomeUrl}`}>
+                        <Image src={objeto.url} alt={objeto.nomeUrl} height={40} width={40}/>
+                        <p className='TextoMapMenuHamburguer'>{objeto.nome}</p>
+                    </Link>
+                )}
+                <div className='SairMenuHamburguer'>
+                    <Image src="/icons/sair.svg" alt='sair' width={40} height={40} />
+                    <p className='SairTexotMenuHamburguer'>Sair</p>
+                </div>
             </div>
             <Image src="/images/samur-principal.png" alt='logo samur' width={150} height={100} className='divIconLogo' />
 
--- src/data/menu-hamburguer.ts
+++ src/data/menu-hamburguer.ts
@@ -1,44 +1,38 @@
-export const menuHamburguer = [
-    {
-        key: 1,
-        nome: "Home",
-        nomeUrl: "",
-        url: "http://localhost:3000/icons/home.svg"
-    },
+export const menuHamburguerData = [
     {
         key: 2,
         nome: "Agendamento",
         nomeUrl: "/agendamento",
-        url: "http://localhost:3000/icons/calendarioMenu.svg"
+        url: "/icons/calendarioMenu.svg"
     },
     {
         key: 3,
         nome: "Documentos",
         nomeUrl: "/documentos",
-        url: "http://localhost:3000/icons/documentosHamburguer.svg"
+        url: "/icons/documentosHamburguer.svg"
     },
     {
         key: 4,
         nome: "Financeiro",
         nomeUrl: "/financeiro",
-        url:"http://localhost:3000/icons/financeiroMenu.svg"
+        url:"/icons/financeiroMenu.svg"
     },
     {
         key: 5,
         nome: "Orçamento",
         nomeUrl: "orçamento",
-        url: "http://localhost:3000/icons/oraçamentoMenu.svg"
+        url: "/icons/oraçamentoMenu.svg"
     },
     {
         key: 6,
         nome: "Prontuário",
         nomeUrl: "prontuario",
-        url: "http://localhost:3000/icons/prontuario.svg"
+        url: "/icons/protuario.svg"
     },
     {
         key: 7,
         nome: "Resultados",
         nomeUrl: "resultados",
-        url: "http://localhost:3000/icons/resultados"
+        url: "/icons/resultados.svg"
     }
 ]
