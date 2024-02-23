44e889ec93e808ef8deb8558903c0c9f50e8fa8b
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-23 09:57:36 -0300

    correção do componete HeaderPrincipal e criação do componente Agendamento

---
+++ src/app/agendamento/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import "./style.css"
+import HeaderPrincipal from "@/components/header"
+export default function Agendamento(){
+    return(
+        <div>
+            <HeaderPrincipal/>
+        </div>
+    )
+}
