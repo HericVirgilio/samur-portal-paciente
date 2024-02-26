d23c13f6c7a969ea4f77d394dbbda8dd18a01586
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-26 08:45:54 -0300

    começando procedimentos-externos

--- src/app/prontuario/procedimentos-externos/page.tsx
+++ src/app/prontuario/procedimentos-externos/page.tsx
@@ -3,10 +3,12 @@ import HeaderPrincipal from "@/components/header"
 import HeaderTable from "@/components/headerTable"
 import Tabela from "@/components/tabela"
 import { AtestadoData } from "@/data/atestado.data"
+import Image from "next/image"
+import "./style.css"
 
-export default function Receitas(){
+export default function ProcedimentosExternos(){
     return(
-        <div>
+        <div className="BoxProcedimentosExternos">
             <HeaderPrincipal/>
             <div style={{ backgroundColor: "var(--branco)", width: "95vw", margin: "20px auto", padding: "20px 0" }}>
                 <h2 style={{ fontWeight: "bold", fontSize: "20px", marginLeft: "10px", marginBottom: "20px" }}>Lista de Receitas</h2>
