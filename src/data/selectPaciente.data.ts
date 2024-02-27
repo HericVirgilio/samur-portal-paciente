d38d7db91cf2d7912bfa789ebf3c675f50f17500
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-27 12:20:57 -0300

    componente de select para paginas filhas de prontuário pronto

---
+++ src/data/selectPaciente.data.ts
@@ -0,0 +1,12 @@
+export const SelectPacienteData:any [] = [
+    {
+        key: 0,
+        nome:"Gregory House",
+        url: "/images/house.webp"
+    },
+    {
+        key: 1,
+        nome:"Stella Vermillion",
+        url: "/images/perfil-icon.png"
+    }
+];
