e2c5eb11afb51346b1235c8f77604a615a88100c
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-27 12:20:57 -0300

    componente de select para paginas filhas de prontuário pronto

---
+++ src/app/prontuario/vacinas/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import HeaderPrincipal from "@/components/header"
+import SelecionePaciente from "@/components/selecionePaciente"
+
+export default function Vacinas(){
+    return(
+        <div>
+            <HeaderPrincipal/>
+            <SelecionePaciente/>
+        </div>
+    )
+}
