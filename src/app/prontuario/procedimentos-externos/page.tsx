5c6bd46c7a308489b66e64ec0d5c65da75e322c2
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-27 08:58:57 -0300

    criação do menu e implementação do pop-up

--- src/app/prontuario/procedimentos-externos/page.tsx
+++ src/app/prontuario/procedimentos-externos/page.tsx
@@ -1,12 +1,26 @@
 "use client"
 import HeaderPrincipal from "@/components/header"
 import PopUp from "@/components/pop-up"
+import HeaderTable from "@/components/headerTable"
+import Tabela from "@/components/tabela"
+import { ResultadosData } from "@/data/resultados.data"
 
-export default function ProcedimentosExternos(){
-    return(
+
+export default function ProcedimentosExternos() {
+    return (
         <div>
-            <HeaderPrincipal/>
-            <PopUp/>
+            <HeaderPrincipal />
+            <PopUp />
+            <div style={{ backgroundColor: "var(--branco)", width: "95vw", margin: "20px auto", padding: "20px 0" }}>
+                <h2 style={{ fontWeight: "bold", fontSize: "20px", marginLeft: "10px", marginBottom: "20px" }}>Procedimentos externos</h2>
+                <HeaderTable />
+                {ResultadosData.map((objeto) => (
+                    <Tabela key={objeto.key}
+                        objeto={objeto}
+                        texto1="Paciente"
+                        texto2="Data" />
+                ))}
+            </div>
         </div>
     )
 }
