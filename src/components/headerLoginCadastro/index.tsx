f6b13f15c1e2aea44d2329b87b02a643f3a2fb3b
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-28 12:44:58 -0300

    refazendo inputs e ajustando erros

--- src/components/headerLoginCadastro/index.tsx
+++ src/components/headerLoginCadastro/index.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Header() {
     return(
-        <div className='m-[30px]'>
-            <Image src="/images/samur-principal.png" alt="" width={150} height={80}/>
+        <div style={{margin: "30px"}}>
+            <Image src="/images/samur-principal.png" alt="" width={150} height={80} priority />
         </div>
     )
 }
