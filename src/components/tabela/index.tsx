d889fda54c2499f4cb85c7cac21d52ba10b2775b
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-02-29 10:11:46 -0300

    iniciando componente consulta e resolvendo bugs de img

--- src/components/tabela/index.tsx
+++ src/components/tabela/index.tsx
@@ -2,23 +2,40 @@ import { GenericoInterfaceTabela } from "@/interface/generico.interface"
 import Image from "next/image"
 import "./style.css"
 import { useState } from "react"
-
 export default function Tabela(props: { objeto: GenericoInterfaceTabela }) {
-    const [menuAberto, setMenuAberto] = useState(false);
 
-    const handleDropDownClick = () => {
-        setMenuAberto(!menuAberto);
+    const [tabela, setTabela] = useState({});
+    const [menuAberto, setMenu] = useState(false)
+
+    const DropDown = () => {
+        setTabela({
+            display: "block"
+        })
+        setMenu(true)
     }
-
+    const FecharDropDown = () => {
+        setTabela({
+            display: "none"
+        })
+        setMenu(false)
+    }
+    const HandleDropDownClick = () => {
+        if (menuAberto == true) {
+            FecharDropDown();
+        } else {
+            DropDown();
+        }
+    }
     return (
         <div>
+
             <div className={props.objeto.key % 2 === 0 ? "linhaPar" : "linhaImpar"} key={props.objeto.key}>
-                <div id="BoxBodyTabela">
+                <div id="BoxBodyTabela" >
                     <p>{props.objeto.nome}</p>
                     <p>{props.objeto.data.toLocaleDateString()}</p>
-                    <Image className="setaParaBaixo" onClick={handleDropDownClick} src="/icons/flecha-drop-down.svg" alt="drop down" height={20} width={20} />
+                    <Image className="setaParaBaixo" key={props.objeto.key} onClick={HandleDropDownClick} src="/icons/flecha-drop-down.svg" alt="drop down" height={20} width={20} />
                 </div>
-                <div className={menuAberto ? "DropDown" : "DropDown hidden"}>
+                <div style={tabela} className="DropDown">
                     <div className="BoxBodyTabelaDropDown">
                         <p>{props.objeto.primeiraColuna}</p>
                         <p>{props.objeto.primeiraColunaRes}</p>
@@ -27,6 +44,7 @@ export default function Tabela(props: { objeto: GenericoInterfaceTabela }) {
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
