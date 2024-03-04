462111ced8f9a5f796cf74b32b30ef86f568c45c
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-03-04 11:19:04 -0300

    alteração tela de lofin template para cpf

--- src/components/input-text-login/index.tsx
+++ src/components/input-text-login/index.tsx
@@ -11,10 +11,22 @@ export default function InputText(){
         console.log("cpf", cpf);
         console.log("senha", senha);
     };
+    const FormataCpf = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+           
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+        }
+        setCpf(value);
+    };
 
     return(
         <form className="div-inputText" onSubmit={Logar}>
-            <input className="item-inputText border border-black rounded" type="text" name="cpfLogin" id="cpfLogin" placeholder="CPF" onChange={(e) => setCpf(e.target.value)}/>
+            <input className="item-inputText border border-black rounded" type="text" name="cpfLogin"
+            maxLength={14} onChange={FormataCpf} placeholder="CPF" value={cpf} 
+            id="cpfLogin"/>
             <input className="item-inputText border border-black rounded" type="password" name="senhaLogin" id="senhaLogin" placeholder="Senha" onChange={(e) => setSenha(e.target.value)}/>
             <input className='botao-entrar-login' type="submit" value="Entrar"/>
         </form>
