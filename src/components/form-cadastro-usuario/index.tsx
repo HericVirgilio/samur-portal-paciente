9431f4b3534e1fdc013d87103b693a9a91c00fa8
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-03-04 11:06:57 -0300

    formatação de data, telefone, cpf, rg e cep

--- src/components/form-cadastro-usuario/index.tsx
+++ src/components/form-cadastro-usuario/index.tsx
@@ -1,12 +1,78 @@
 import "./style.css"
+import { useState } from "react"
 
-/*Colocar os id iqual pode gerar conflito no futuro lembre-se de verificar caso resulte erro*/
 export default function FormCadastroUsuario() {
+
+    const[cpf,setCpf] = useState<string>("")
+    const[data,setData] = useState<string>("")
+    const[rg,setRg] = useState<string>("")
+    const[cep,setCep] = useState<string>("")
+    const[telefone,setTelefone] = useState<string>("")
+    const[telefone2,setTelefone2] = useState<string>("")
+
+
+    const FormataCpf = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+           
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+        }
+        setCpf(value);
+    };
+    const FormataRg = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+           
+            value = value.replace(/(\d{2})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{2})(\d{1,2})$/, "$1-$2");
+        }
+        setRg(value);
+    };
+    const FormataCep = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+            value = value.replace(/(\d{5})(\d{1,2})$/, "$1-$2");
+        }
+        setCep(value);
+    };
+    const FormataTelefone = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+            value = value.replace(/(\d{0})(\d)/, "$1($2");
+            value = value.replace(/(\d{2})(\d)/, "$1)$2");
+            value = value.replace(/(\d{5})(\d{4,5})$/, "$1-$2");
+        }
+        setTelefone(value);
+    };
+    const FormataTelefone2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+            value = value.replace(/(\d{0})(\d)/, "$1($2");
+            value = value.replace(/(\d{2})(\d)/, "$1)$2");
+            value = value.replace(/(\d{5})(\d{4,5})$/, "$1-$2");
+        }
+        setTelefone2(value);
+    };
+    const FormataData = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, ""); 
+        if (value.length <= 11) {
+            value = value.replace(/(\d{2})(\d)/, "$1/$2");
+            value = value.replace(/(\d{2})(\d)/, "$1/$2");
+            value = value.replace(/(\d{5})(\d{4,5})$/, "$1-$2");
+        }
+        setData(value);
+    };
+
+   
+
     return (
         <form className="FormularioUser">
 
-            <input className="InputBox" type="text" name="cpf" id="cpf"
-                placeholder="CPF" />
+            <input className="InputBox" type="text" name="cpf" id="cpf" maxLength={14} onChange={FormataCpf}
+                placeholder="CPF" value={cpf} />
 
             <input className="InputBox" type="text" name="nomeCompleto" id="nomeCompleto"
                 placeholder="Nome Completo" />
@@ -14,11 +80,12 @@ export default function FormCadastroUsuario() {
             <input className="InputBox" type="email" name="email" id="email"
                 placeholder="Email" />
 
-            <input className="InputBox" type="text" name="dataNasc" id="dataNasc"
-                placeholder="Data de nascimento" />
+            <input className="InputBox" type="text" name="dataNasc" id="dataNasc" 
+            maxLength={10} onChange={FormataData} value={data}
+                placeholder="Data de nascimento"/>
 
-            <input className="InputBox" type="text" name="rg" id="rg"
-                placeholder="RG" />
+            <input className="InputBox" type="text" name="rg" id="rg" maxLength={13} onChange={FormataRg}
+                value={rg} placeholder="RG" />
 
             <input className="InputBox" type="text" name="nacionalidade" id="nacionalidade"
                 placeholder="Nacionalidade" />
@@ -43,7 +110,7 @@ export default function FormCadastroUsuario() {
 
             <h2 style={{margin: "20px 0 20px 0"}}>Endereço</h2>
 
-            <input style={{ marginTop: "10px" }} className="InputBox" type="text" name="cep" id="cep" placeholder="Cep" />
+            <input style={{ marginTop: "10px" }} className="InputBox" type="text" name="cep" id="cep" placeholder="Cep" maxLength={9} onChange={FormataCep} value={cep} />
 
             <input className="InputBox" type="text" name="logradouro" id="logradouro" placeholder="Logradouro" />
 
@@ -59,10 +126,10 @@ export default function FormCadastroUsuario() {
             <input className="InputBox" type="text" name="uf" id="uf" placeholder="UF" />
 
             <input className="InputBox" type="text" name="telefone1" id="telefone1"
-                placeholder="Telefone 1" />
+                placeholder="Telefone 1" maxLength={14} onChange={FormataTelefone} value={telefone} />
 
             <input className="InputBox" type="text" name="telefone2" id="telefone2"
-                placeholder="Telefone 2" />
+                placeholder="Telefone 2" maxLength={14} onChange={FormataTelefone2} value={telefone2} />
 
             <label className='item-inputText'>
                 <input type="checkbox" name="politica-privacidade" id="politica-privacidade" />
