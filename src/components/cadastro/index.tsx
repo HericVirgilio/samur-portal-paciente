512ff57705055cb9831809f59fd64eb063ef200c
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-03-05 12:48:32 -0300

    tela cadastro 1/3 completo

--- src/components/cadastro/index.tsx
+++ src/components/cadastro/index.tsx
@@ -1,6 +1,9 @@
 import "./style.css"
 import { FormEvent, useState } from "react";
 import { FormData } from "@/interface/form-data.interface";
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 interface Props {
     sendDataToParent: (data: FormData) => void;
@@ -12,12 +15,13 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
     const [senha, setSenha] = useState<string>("")
     const [senha2, setSenha2] = useState<string>("")
     const [erroSenha, setErroSenha] = useState<string>("")
+    const [showPassword, setShowPassword] = useState(false);
 
     const EnviarFormulario = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const DivErro = document.querySelector(".DivErro");
         if (senha === senha2 && validarSenha(senha)) {
             sendDataToParent({ email, senha });
+            console.log("Formulario enviado")
         } else if (senha != senha2) {
             setErroSenha("As senhas são diferentes")
         } else {
@@ -30,26 +34,71 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
         return regex.test(senha);
     }
 
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
     return (
         <div className="BoxForm">
             <div className="DivCadastroTitle">
                 <h2>Cadastro</h2>
                 <p>1/3</p>
             </div>
-            <form className="DivCadastroForm" onSubmit={EnviarFormulario}>
-                <input type="email" placeholder="Email" className="InputCadastro" id="InputPlaceholders"
-                    value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Senha" className="InputCadastro" id="InputPlaceholders"
-                    value={senha} onChange={(e) => setSenha(e.target.value)} />
-                <input type="password" placeholder="Digite a senha novamente" className="InputCadastro" id="InputPlaceholders"
-                    value={senha2} onChange={(e) => setSenha2(e.target.value)} />
-                    
-                <div>
-                    {erroSenha && <div className="DivErro">{erroSenha}</div>}
-                </div>
-
-                <input type="submit" id="InputSenha" value="Continuar" className="InputCadastro" />
+            <form className="DivCadastroForm" onSubmit={EnviarFormulario} >
+            <FormControl sx={{ m: 1, width: '80vw'}} variant="outlined">
+                    <InputLabel  htmlFor="outlined-adornment-password">Email</InputLabel>
+                    <OutlinedInput value={email} onChange={(e) => setEmail(e.target.value)}
+                        id="outlined-adornment-password"
+                        type={'email'}
+                        label="Email" />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '80vw'}} variant="outlined">
+                    <InputLabel  htmlFor="outlined-adornment-password">Senha</InputLabel>
+                    <OutlinedInput value={senha} onChange={(e) => setSenha(e.target.value)}
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        } label="Senha" />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '80vw' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Digite a senha novamente</InputLabel>
+                    <OutlinedInput value={senha2} onChange={(e) => setSenha2(e.target.value)}
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        } label="Digite a senha novamente" />
+                </FormControl>
+                {erroSenha && <div className="DivErro" >{erroSenha}</div>}
+                <input type="submit" value="Continuar" className="InputCadastro" id="InputSenha" />
             </form>
+            <div className="RequisitosSenha">
+                    <h5>A senha deve conter:</h5>
+                    
+                    <p>Letras e números</p>
+                    <p>No minimo 5 caracteres</p>
+                </div>
         </div>
     )
 }
