d608215bbce4b2c4cb42ae2f8439e61a75a3eb5c
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-12 10:02:28 -0300

    cadastro etapa 1 /3 concluido com verificação de senha

--- src/components/cadastro/index.tsx
+++ src/components/cadastro/index.tsx
@@ -1,10 +1,11 @@
 import "./style.css"
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { FormData } from "@/interface/form-data.interface";
 import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from "@mui/material";
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import BotaoEnviarFormulario from "../botao-login-cadastro";
+import Image from "next/image";
 
 interface Props {
     sendDataToParent: (data: FormData) => void;
@@ -17,10 +18,13 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
     const [senha2, setSenha2] = useState<string>("")
     const [erroSenha, setErroSenha] = useState<string>("")
     const [showPassword, setShowPassword] = useState(false);
+    const [regexNumero, setRegexNumero] = useState<boolean | null>(null)
+    const [regexCaracterEspecial, setRegesCaracterEspecial] = useState<boolean | null>(null)
+    const [regexLetras, setRegexLetras] = useState<boolean | null>(null)
 
     const EnviarFormulario = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (senha === senha2 && validarSenha(senha)) {
+        if (senha === senha2 && validarSenha()) {
             sendDataToParent({ email, senha });
         } else if (senha != senha2) {
             setErroSenha("As senhas são diferentes")
@@ -29,9 +33,53 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
         }
     }
 
-    const validarSenha = (senha: string) => {
-        const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&_-]{5,}$/;
-        return regex.test(senha);
+    useEffect(() => {
+        if (senha !== "") {
+            validarSenha()
+            verificaNumero()
+            verificaCaracteres()
+            verificaLetras()
+        }
+    }, [senha])
+
+    const validarSenha = () => {
+        if (senha.length >= 8) {
+            verificaNumero()
+            verificaCaracteres()
+            verificaLetras()
+            console.log("numero:", regexNumero, "caracteres: ", regexCaracterEspecial, "letras: ", regexLetras)
+            if(regexNumero && regexLetras && regexCaracterEspecial){
+                return true
+            }
+        } else {
+            console.log("senha menor que  8 seu tamanho é " + senha.length)
+            return false
+        }
+    }
+
+    const verificaNumero = () => {
+        const regexNumero = /\d/
+        if (regexNumero.test(senha)) {
+            setRegexNumero(true)
+        } else {
+            setRegexNumero(false)
+        }
+    }
+    const verificaCaracteres = () => {
+        const regexCaracteresEspeciais = /[^a-zA-Z 0-9]+/g
+        if (regexCaracteresEspeciais.test(senha)) {
+            setRegesCaracterEspecial(true)
+        } else {
+            setRegesCaracterEspecial(false)
+        }
+    }
+    const verificaLetras = () => {
+        const regexLetras = /[A-Za-z]/
+        if (regexLetras.test(senha)) {
+            setRegexLetras(true)
+        } else {
+            setRegexLetras(false)
+        }
     }
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -43,12 +91,12 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
     return (
         <div className="BoxForm">
             <form className="DivCadastroForm" onSubmit={EnviarFormulario} >
-                <FormControl sx={{ m: 1, width: '80vw'}} variant="outlined">
+                <FormControl sx={{ m: 1, width: '80vw' }} variant="outlined">
                     <InputLabel htmlFor="outlined-adornment-password" size="small">Email</InputLabel>
                     <OutlinedInput value={email} onChange={(e) => setEmail(e.target.value)}
                         type={'email'}
-                        label="Email" 
-                        size="small"/>
+                        label="Email"
+                        size="small" />
                 </FormControl>
                 <FormControl sx={{ m: 1, width: '80vw' }} variant="outlined">
                     <InputLabel htmlFor="outlined-adornment-password" size="small" >Senha</InputLabel>
@@ -68,9 +116,9 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
                         } label="Senha" />
                 </FormControl>
                 <FormControl sx={{ m: 1, width: '80vw' }} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password"  size="small">Digite a senha novamente</InputLabel>
+                    <InputLabel htmlFor="outlined-adornment-password" size="small">Digite a senha novamente</InputLabel>
                     <OutlinedInput value={senha2} onChange={(e) => setSenha2(e.target.value)}
-                         size="small"
+                        size="small"
                         type={showPassword ? 'text' : 'password'}
                         endAdornment={
                             <InputAdornment position="end">
@@ -86,13 +134,30 @@ export default function CadastroFormulario({ sendDataToParent }: Props) {
                         } label="Digite a senha novamente" />
                 </FormControl>
                 {erroSenha && <div className="DivErro" >{erroSenha}</div>}
-                <BotaoEnviarFormulario text="Continuar"/>
+                <BotaoEnviarFormulario text="Continuar" />
             </form>
             <div className="RequisitosSenha">
                 <h5>A senha deve conter:</h5>
-
-                <p>Letras e números</p>
-                <p>No minimo 5 caracteres</p>
+                <div className="ContainerVeridicadoresDeSenha">No minimo 8 caracteres
+                    <Image src={"/icons/check.svg"} alt="" width={20} height={20}
+                        style={{ display: senha.length >= 8 ? "block" : "none" }} />
+                    <p style={{ display: senha.length < 8 && senha !== "" ? "block" : "none" }}>X</p>
+                </div>
+                <div className="ContainerVeridicadoresDeSenha">Letras
+                    <Image src={"/icons/check.svg"} alt="" width={20} height={20}
+                        style={{ display: regexLetras ? "block" : "none" }} />
+                    <p style={{ display: regexLetras === false ? "block" : "none" }}>X</p>
+                </div>
+                <div className="ContainerVeridicadoresDeSenha">Números
+                    <Image src={"/icons/check.svg"} alt="" width={20} height={20}
+                        style={{ display: regexNumero ? "block" : "none" }} />
+                    <p style={{ display: regexNumero === false ? "block" : "none" }}>X</p>
+                </div>
+                <div className="ContainerVeridicadoresDeSenha">Ao menos 1 caractere especial
+                    <Image src={"/icons/check.svg"} alt="" width={20} height={20}
+                        style={{ display: regexCaracterEspecial ? "block" : "none" }} />
+                    <p style={{ display: regexCaracterEspecial === false ? "block" : "none" }}>X</p>
+                </div>
             </div>
         </div>
     )
