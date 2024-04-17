4f68af75a5e8f65f651a2719b62f86bd0bc4e7e2
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-16 22:44:34 -0300

    correções build

--- src/app/criar-nova-senha/page.tsx
+++ src/app/criar-nova-senha/page.tsx
@@ -1,11 +1,7 @@
-import Header from "@/components/header-login-cadastro"
-
 export default function CriarNovaSenha(){
     return(
         <div>
-            <Header/>
             <h2>Criar nova senha</h2>
-            
         </div>
     )
 }
