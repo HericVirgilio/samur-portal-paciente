72a2edb83f56a5ff9e6a7ebab53c0dd8e763c2b8
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-22 12:30:44 -0300

    alterando minha conta e formatando para desktop

--- src/app/minha-conta/page.tsx
+++ src/app/minha-conta/page.tsx
@@ -12,15 +12,17 @@ export default function MinhaConta (){
                 <Image src={"/images/eduarda-ferreira.png"} alt="" width={100} height={100} className="ImagemUser"/>
                 <h3>Eduarda Ferreira</h3>
             </div>
-            {ComponentesMinhaConta.map((objeto) => 
-                <Link href={objeto.url} className="ContainerIcons" key={objeto.id}>
-                    <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
-                        <Image src={objeto.img} alt={objeto.titulo} width={40} height={40}/>
-                        <h4>{objeto.titulo}</h4>
-                    </div>
-                    <Image style={{transform: "rotate(270deg)"}} src={"/icons/flecha-drop-down.svg"} alt="DropDown" width={30} height={30}/>
-                </Link>
-            )}
+            <div className="Container">
+                {ComponentesMinhaConta.map((objeto) =>
+                    <Link href={objeto.url} className="ContainerIcons" key={objeto.id}>
+                        <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
+                            <Image src={objeto.img} alt={objeto.titulo} width={40} height={40}/>
+                            <h4>{objeto.titulo}</h4>
+                        </div>
+                        <Image style={{transform: "rotate(270deg)"}} src={"/icons/flecha-drop-down.svg"} alt="DropDown" width={30} height={30}/>
+                    </Link>
+                )}
+            </div>
         </div>
     )
 }
