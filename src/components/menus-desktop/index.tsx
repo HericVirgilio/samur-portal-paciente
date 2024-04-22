5ee90d2e54c8b39ac6ded93d9434b7e24894ba45
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-22 08:34:18 -0300

    implementando rota no menu navbar

--- src/components/menus-desktop/index.tsx
+++ src/components/menus-desktop/index.tsx
@@ -1,22 +1,28 @@
 import "./style.css"
 import { menuHamburguerData } from "@/data/menu-hamburguer.data"
 import Link from "next/link"
-export default function MenuDesktop (){
-    return(
+import { useRouter } from "next/navigation"
+export default function MenuDesktop() {
+
+    const router = useRouter();
+
+    const IrPara = (url: string) => {
+        router.push(url)
+    }
+    return (
         <div className="ContainerMenuDesktop">
             <div className="ContainerIcones">
                 <div className="ContainerAtalhosMenuHamburguer">
                     <ul className="AtalhosMenuHamburguer">
-                        {menuHamburguerData.map((objeto, index) => 
-                            index !== menuHamburguerData.length - 1 &&(
-                            <Link key={objeto.key} href={objeto.nomeUrl}>
-                                <li>{objeto.nome}</li>
-                            </Link>)
+                        {menuHamburguerData.map((objeto, index) =>
+                            index !== menuHamburguerData.length - 1 && (
+                                <li key={objeto.key} onClick={() => IrPara(objeto.nomeUrl)}>{objeto.nome}</li>
+                            )
                         )}
                     </ul>
                 </div>
                 <div className="NovoAgendamento">
-                    
+
                     <span>Novo Agendamento</span>
                 </div>
             </div>
